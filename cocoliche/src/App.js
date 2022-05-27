7ad8ed6f667ae7be88788f3fc63d5cc4a55d884b
rochi <haitrochi@gmail.com>
2022-05-26 23:13:54 -0300

    wip

--- cocoliche/src/App.js
+++ cocoliche/src/App.js
@@ -5,13 +5,34 @@ import NavBar from './components/NavBar/NavBar'
 import ItemList from './components/ItemListContainer/ItemListContainer';
 import Grid from '@mui/material/Grid';
 import ItemCount from './components/ItemCount/ItemCount';
-
+import { useState, useEffect} from 'react';
 
 function App() {
- 
+  const [producto, setProducts] = useState([])
   let styleCustom = {
     color : '#C7CF99'
   }
+
+  const getProducts =() => {
+    return new Promise( (resolve, reject) =>{
+        setTimeout(() => {
+            resolve(producto)
+        }, 2000)
+    })
+  }
+  useEffect( () =>{
+    getProducts()
+    .then ( (response) =>{
+        console.log("respuesta: ", response)
+        setProducts (response)
+    })
+    .catch( (err) => {
+        console.log("error")
+    })
+    .finally( () => {
+        console.log("finally: finalizo ")
+    })
+  }, [])
 
 
   return (
--- cocoliche/src/components/ItemListContainer/ItemListContainer.js
+++ cocoliche/src/components/ItemListContainer/ItemListContainer.js
@@ -1,23 +1,84 @@
 import React from "react"
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import {useState, useEffect} from 'react';
 
 
-const ItemList = (props) => {
-    return (
-        <Card>
-        <CardContent>
-        <div className="item-list">
-            <h2>Velas de Soja</h2>
-            <div>
-                <img src={'./${image}'} alt={"producto"}/>
+const ItemList = ({product}) => {
+    const [products, setProducts] = useState([])
+    const producto = [
+        {
+            title: "Velas de Soja",
+            image: 'product4.jpeg',
+            price: 2000,
+            stock: 7,
+            id: 1
+        },
+        {
+            title: "Velas de Soja",
+            image: 'product4.jpeg',
+            price: 2000,
+            stock: 10,
+            id: 2
+        },
+        {
+            title: "Velas de Soja",
+            image: 'product4.jpeg',
+            price: 2000,
+            stock: 8,
+            id: 3
+        },
+    ]
+    const getProducts =() => {
+        return new Promise( (resolve, reject) =>{
+            setTimeout(() => {
+                resolve(producto)
+            }, 2000)
+        })
+    }
+    useEffect( () =>{
+        getProducts()
+        .then ( (response) =>{
+            console.log("respuesta: ", response)
+            setProducts (response)
+        })
+        .catch( (err) => {
+            console.log("error")
+        })
+        .finally( () => {
+            console.log("finally: finalizo ")
+        })
+    }, [])
+
+    if (product){
+        return (
+            <Card>
+                <h2>{product} </h2>
+                {console.log("products: ", products)}
+            <CardContent>
+            <div className="item-list">
+              <Grid container spacing={4}>
+              {
+                     products.map( ({producto}) =>{
+                         return( 
+                            <Grid item md={3}>
+                            <ItemList title={producto.title} image={producto.image} price={producto.price} stock={producto.stock}/>
+                            </Grid>
+                         )
+                    })
+                }
+                 
+              </Grid>
             </div>
-            <p>{props.information}</p>
-            <span> $2000</span>
-        </div>
+    
+            </CardContent>
+            </Card>
+           
+            
+        )
+        
+    }
 
-        </CardContent>
-        </Card>
-    )
 }
 export default ItemList
