8207ec75c2537dd19077a4b7cd65dedc81317dab
rochi <haitrochi@gmail.com>
2022-05-26 23:32:45 -0300

    wip

--- cocoliche/src/components/ItemListContainer/ItemListContainer.js
+++ cocoliche/src/components/ItemListContainer/ItemListContainer.js
@@ -3,37 +3,16 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import {useState, useEffect} from 'react';
+import {productos} from './../Items/ItemList';
 
 
-const ItemList = ({product}) => {
+const ItemList = ({productos}) => {
     const [products, setProducts] = useState([])
-    const producto = [
-        {
-            title: "Velas de Soja",
-            image: 'product4.jpeg',
-            price: 2000,
-            stock: 7,
-            id: 1
-        },
-        {
-            title: "Velas de Soja",
-            image: 'product4.jpeg',
-            price: 2000,
-            stock: 10,
-            id: 2
-        },
-        {
-            title: "Velas de Soja",
-            image: 'product4.jpeg',
-            price: 2000,
-            stock: 8,
-            id: 3
-        },
-    ]
+ 
     const getProducts =() => {
         return new Promise( (resolve, reject) =>{
             setTimeout(() => {
-                resolve(producto)
+                resolve(products)
             }, 2000)
         })
     }
@@ -51,19 +30,18 @@ const ItemList = ({product}) => {
         })
     }, [])
 
-    if (product){
         return (
             <Card>
-                <h2>{product} </h2>
+                <h2>{productos} </h2>
                 {console.log("products: ", products)}
             <CardContent>
             <div className="item-list">
               <Grid container spacing={4}>
               {
-                     products.map( ({producto}) =>{
+                     products.map( ({productos}) =>{
                          return( 
                             <Grid item md={3}>
-                            <ItemList title={producto.title} image={producto.image} price={producto.price} stock={producto.stock}/>
+                            <ItemList title={productos.title} image={productos.image} price={productos.price} stock={productos.stock}/>
                             </Grid>
                          )
                     })
@@ -78,7 +56,7 @@ const ItemList = ({product}) => {
             
         )
         
-    }
+    
 
 }
 export default ItemList
---
+++ cocoliche/src/components/Items/ItemList.js
@@ -0,0 +1,25 @@
+const productos = [
+    {
+        title: "Velas de Soja",
+        image: 'product4.jpeg',
+        price: 2000,
+        stock: 7,
+        id: 1
+    },
+    {
+        title: "Velas de Soja",
+        image: 'product4.jpeg',
+        price: 2000,
+        stock: 10,
+        id: 2
+    },
+    {
+        title: "Velas de Soja",
+        image: 'product4.jpeg',
+        price: 2000,
+        stock: 8,
+        id: 3
+    },
+]
+
+export default productos
